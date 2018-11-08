5950c63803377be81ac59ed8430894eaf617333e
Cnfc <stdashkov@mail.ru>
2018-11-08 18:23:58 +0300

    add Layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import App from '../../App';
+
+
+
+const layout = () => (
+    <Aux>
+   
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {/* {props.children} */}
+    
+        </main>
+    </Aux>
+);
+
+export default layout;
