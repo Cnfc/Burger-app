cccab2e93d5443588a74eab76ca6eff90617b203
Cnfc <stdashkov@mail.ru>
2018-11-17 17:57:46 +0300

    global error handler HOC

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummury from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 import axios from '../../axios-orders';
 
@@ -104,15 +105,15 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'fastest'
         }
-        // axios.post('/orders.json', order)
-        //     .then(res => {
-        //         console.log(res);
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(err => {
-        //         console.log(err)
-        //         this.setState({loading: false, purchasing: false});
-        //     });
+        axios.post('/orders.json', order)
+            .then(res => {
+                console.log(res);
+                this.setState({loading: false, purchasing: false});
+            })
+            .catch(err => {
+                console.log(err)
+                this.setState({loading: false, purchasing: false});
+            });
     }
 
     render () {
@@ -154,4 +155,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
